282a172d331d46ad9f7e7d77f99884dd19efdd8e
Kirill Puljavin <105564583+KirillPuljavin@users.noreply.github.com>
2024-10-22 15:16:41 +0200

    git pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/username.github.io/", // Replace with your username.github.io for root URL
+  base: "/kirillpuljavin.github.io/",
 });
